e7dea7c73cd109624bec92d29ec2e26ffa36f41b
myselfasghar <120890667+myselfasghar@users.noreply.github.com>
2023-02-17 09:12:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,86 @@
+let x=document.getElementById('login');
+let y=document.getElementById('register');
+let z=document.getElementById('btn');
+function register()
+{
+  x.style.left='-400px';
+  y.style.left='50px';
+  z.style.left='125px';
+}
+function login()
+{
+  x.style.left='50px';
+  y.style.left='450px';
+  z.style.left='0px';
+}
+// let a = document.getElementById("log")
+// let b = document.getElementById("login")
+//    a.addEventListener('click',()=>{
+//    b.style.visibility = "hidden"
+// })
+
+
+//banner
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
+// script for image slider
+let swiper = new Swiper(".slide-container", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    sliderPerGroup: 4,
+    loop: true,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1000: {
+        slidesPerView: 4,
+      },
+    },
+  });
+   
